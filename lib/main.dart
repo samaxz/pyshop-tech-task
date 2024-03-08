4a31200d68faa4939aa397103774b3b53e43f13f
shamil <samax.zer@gmail.com>
2024-03-08 13:13:13 +0300

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,14 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-// void main() {
-//   runApp(const MyApp());
-// }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Camera app tech task',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -40,79 +37,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   late CameraController controller;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   // Future.microtask(() async {
-  //   //   // seems like this can't be initialized
-  //   //   _cameras = await availableCameras();
-  //   //   controller = CameraController(
-  //   //     _cameras[0],
-  //   //     // _cameras[1],
-  //   //     ResolutionPreset.medium,
-  //   //     // imageFormatGroup: ImageFormatGroup.yuv420,
-  //   //   );
-  //   //   try {
-  //   //     await controller?.initialize();
-  //   //   } catch (e) {
-  //   //     if (e is CameraException) {
-  //   //       switch (e.code) {
-  //   //         case 'CameraAccessDenied':
-  //   //           // Handle access errors here.
-  //   //           break;
-  //   //         default:
-  //   //           // Handle other errors here.
-  //   //           break;
-  //   //       }
-  //   //     }
-  //   //   }
-  //   // });
-  //   controller = CameraController(
-  //     _cameras[0],
-  //     // _cameras[1],
-  //     ResolutionPreset.medium,
-  //     // imageFormatGroup: ImageFormatGroup.yuv420,
-  //   );
-  //   try {
-  //     controller?.initialize();
-  //   } catch (e) {
-  //     if (e is CameraException) {
-  //       switch (e.code) {
-  //         case 'CameraAccessDenied':
-  //           // Handle access errors here.
-  //           break;
-  //         default:
-  //           // Handle other errors here.
-  //           break;
-  //       }
-  //     }
-  //   }
-  // }
-
-  // Future<void> initCamera() async {
-  //   // _cameras = await availableCameras();
-  //   controller = CameraController(
-  //     _cameras[0],
-  //     ResolutionPreset.medium,
-  //     // imageFormatGroup: ImageFormatGroup.yuv420,
-  //   );
-  //   await controller.initialize();
-  //   // if (!mounted) return;
-  //   // setState(() {});
-  // }
-
   @override
   void initState() {
     super.initState();
-    // controller = CameraController(
-    //   _cameras[0],
-    //   ResolutionPreset.medium,
-    //   imageFormatGroup: ImageFormatGroup.yuv420,
-    // )..initialize();
-    // Future.microtask(initCamera);
     controller = CameraController(
       _cameras[0],
-      ResolutionPreset.medium,
+      ResolutionPreset.max,
     );
     controller.initialize().then((_) {
       if (!mounted) return;
@@ -157,35 +87,30 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('title'),
-      ),
+      // body: !controller.value.isInitialized ? Container() : CameraPreview(controller),
+      // body: !controller.value.isInitialized
+      //     ? Container()
+      //     : FittedBox(
+      //         fit: BoxFit.contain,
+      //         child: CameraPreview(controller),
+      //       ),
+      // body: !controller.value.isInitialized
+      //     ? Container()
+      //     : AspectRatio(
+      //         aspectRatio: 1 / controller.value.aspectRatio,
+      //         child: CameraPreview(controller),
+      //       ),
       body: !controller.value.isInitialized
           ? Container()
-          : AspectRatio(
-              aspectRatio: controller.value.aspectRatio,
-              child: CameraPreview(controller),
+          : SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              // this works, while fitted box throws
+              child: Expanded(
+                // fit: BoxFit.fitHeight,
+                child: CameraPreview(controller),
+              ),
             ),
-      // body: controller == null ? Container() : CameraPreview(controller!),
-      // body: CameraPreview(controller!),
-      // body: FutureBuilder(
-      //   future: initCamera(),
-      //   builder: (context, snapshot) {
-      //     if (snapshot.connectionState == ConnectionState.waiting) {
-      //       return const Center(
-      //         child: CircularProgressIndicator(),
-      //       );
-      //     } else if (snapshot.hasError) {
-      //       return const Center(
-      //         child: Text('error happened'),
-      //       );
-      //     }
-      //     return CameraPreview(controller);
-      //   },
-      // ),
-      // body: Container(),
     );
-    // return CameraPreview(controller);
   }
 }
