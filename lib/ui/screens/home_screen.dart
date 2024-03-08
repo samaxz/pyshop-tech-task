6f8a72982e81566daf1ceae7755316cc21a1606b
shamil <samax.zer@gmail.com>
2024-03-08 17:24:35 +0300

    removed useless comments from home_screen.dart

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -11,32 +11,7 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
-  // late CameraController controller;
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   controller = CameraController(
-  //     _cameras[0],
-  //     ResolutionPreset.max,
-  //   );
-  //   controller.initialize().then((_) {
-  //     if (!mounted) return;
-  //     setState(() {});
-  //   }).catchError((Object e) {
-  //     if (e is CameraException) {
-  //       switch (e.code) {
-  //         case 'CameraAccessDenied':
-  //           // Handle access errors here.
-  //           break;
-  //         default:
-  //           // Handle other errors here.
-  //           break;
-  //       }
-  //     }
-  //   });
-  // }
-
+  // TODO make use of this or delete it
   // UPD commented this out for now
   // @override
   // void didChangeAppLifecycleState(AppLifecycleState state) {
@@ -54,72 +29,8 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   //   }
   // }
 
-  // @override
-  // void dispose() {
-  //   controller.dispose();
-  //   super.dispose();
-  // }
-
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   body: !controller.value.isInitialized
-    //       ? Container()
-    //       : Stack(
-    //           children: [
-    //             SizedBox(
-    //               height: MediaQuery.of(context).size.height,
-    //               width: MediaQuery.of(context).size.width,
-    //               // wrapping expanded inside column, so that it
-    //               // doesn't write anything to the console
-    //               child: Column(
-    //                 children: [
-    //                   // this works, while fitted box throws
-    //                   Expanded(
-    //                     child: CameraPreview(controller),
-    //                   ),
-    //                 ],
-    //               ),
-    //             ),
-    //             Padding(
-    //               padding: const EdgeInsets.only(
-    //                 left: 15,
-    //                 bottom: 30,
-    //               ),
-    //               child: Align(
-    //                 alignment: Alignment.bottomLeft,
-    //                 child: Material(
-    //                   shape: const CircleBorder(),
-    //                   // this is for the splash to not go behind
-    //                   // the child's constraints
-    //                   clipBehavior: Clip.antiAlias,
-    //                   child: InkWell(
-    //                     // TODO implement onTap()
-    //                     onTap: () {},
-    //                     // TODO implement onLongPress()
-    //                     onLongPress: () {},
-    //                     splashColor: Colors.grey,
-    //                     child: Container(
-    //                       width: 76,
-    //                       height: 76,
-    //                       padding: const EdgeInsets.all(5),
-    //                       child: Container(
-    //                         alignment: Alignment.center,
-    //                         decoration: BoxDecoration(
-    //                           shape: BoxShape.circle,
-    //                           border: Border.all(
-    //                             color: Colors.black,
-    //                           ),
-    //                         ),
-    //                       ),
-    //                     ),
-    //                   ),
-    //                 ),
-    //               ),
-    //             ),
-    //           ],
-    //         ),
-    // );
     final controller = ref.watch(cameraControllerProvider);
     return controller.when(
       data: (controller) => Scaffold(
@@ -185,6 +96,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       error: (error, stackTrace) => const Center(
         child: Text('error'),
       ),
+      // this is pretty much useless
       loading: () => const Center(
         child: CircularProgressIndicator(),
       ),
