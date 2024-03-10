29ca3dca4495ea037f331dd16a9fcb3e8bf0f9d9
shamil <samax.zer@gmail.com>
2024-03-10 14:23:32 +0300

    added camera permission and changed ui

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:pyshop_task_camera_app/logic/controllers/providers.dart';
-import 'package:pyshop_task_camera_app/ui/widgets/photo_button.dart';
+import 'package:pyshop_task_camera_app/ui/widgets/comment_section.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -30,33 +31,60 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   //   }
   // }
 
+  // this needs to get called only in case access was either denied
+  // for once or permanently
+  Future<void> getCameraPermission() async {
+    final status = await Permission.camera.request();
+    if (status == PermissionStatus.denied ||
+        status == PermissionStatus.permanentlyDenied) {
+      await Permission.camera.request();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(getCameraPermission);
+  }
+
   @override
   Widget build(BuildContext context) {
     final controller = ref.watch(cameraControllerProvider);
     return controller.when(
       data: (controller) => Scaffold(
+        resizeToAvoidBottomInset: false,
+        // resizeToAvoidBottomPadding: false,
         body: !controller.value.isInitialized
             ? const Center(
                 child: CircularProgressIndicator(),
               )
-            : Stack(
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
-                    // wrapping expanded inside column, so that it
-                    // doesn't write anything to the console
-                    child: Column(
-                      children: [
-                        // this works, while fitted box throws
-                        Expanded(
-                          child: CameraPreview(controller),
-                        ),
-                      ],
-                    ),
+            : SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height,
                   ),
-                  const PhotoButton(),
-                ],
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        // wrapping expanded inside column, so that it
+                        // doesn't write anything to the console
+                        child: Column(
+                          children: [
+                            // this works, while fitted box throws
+                            Expanded(
+                              child: CameraPreview(controller),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // TODO remove this
+                      // const PhotoButton(),
+                      const CommentSection(),
+                    ],
+                  ),
+                ),
               ),
       ),
       error: (error, stackTrace) => const Center(
