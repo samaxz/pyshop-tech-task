f64de396a5bd81beb25414a4ddfeb759e6f71dc1
samax <samax.zer@gmail.com>
2024-03-30 16:34:17 +0300

    changed comment section

--- lib/ui/widgets/comment_section.dart
+++ lib/ui/widgets/comment_section.dart
@@ -9,46 +9,36 @@ class CommentSection extends ConsumerStatefulWidget {
 }
 
 class _CommentSectionState extends ConsumerState<CommentSection> {
-  final controller = TextEditingController();
+  late final TextEditingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 15,
-        right: 90,
-        bottom: 30,
-      ),
-      child: Align(
-        alignment: Alignment.bottomLeft,
-        child: Material(
-          // this is for the splash to not go behind
-          // the child's constraints
-          clipBehavior: Clip.antiAlias,
-          child: InkWell(
-            // TODO implement onTap()
-            onTap: () {},
-            // TODO implement onLongPress()
-            onLongPress: () {},
-            splashColor: Colors.grey,
-            child: TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-                filled: true,
-                hintStyle: TextStyle(
-                  color: Colors.grey[700],
-                ),
-                hintText: 'Add a caption...',
-                fillColor: Colors.white70,
-              ),
-            ),
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
           ),
         ),
+        filled: true,
+        hintStyle: TextStyle(
+          color: Colors.grey[700],
+        ),
+        hintText: 'Add a caption...',
+        fillColor: Colors.white,
       ),
     );
   }
