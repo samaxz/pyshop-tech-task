eae8c8ed3bda581642ea0534142f0890426e89cc
samax <samax.zer@gmail.com>
2024-03-30 16:34:07 +0300

    changed send button

--- lib/ui/widgets/send_button.dart
+++ lib/ui/widgets/send_button.dart
@@ -7,25 +7,26 @@ class SendButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Center(
-      child: Material(
-        // to hide background color and corners
-        color: Colors.transparent,
-        // this is for the splash to not go behind
-        // the child's constraints
-        clipBehavior: Clip.antiAlias,
-        child: InkWell(
-          // TODO implement onTap()
-          onTap: () {},
-          // TODO implement onLongPress()
-          onLongPress: () {},
-          splashColor: Colors.grey,
+      // this makes the circle avatar to take full size
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: CircleAvatar(
+          // this makes the splash take full icon size
           child: SizedBox(
+            height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
-            // flex: 3,
-            child: CircleAvatar(
-              child: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.send_outlined),
+            child: Material(
+              color: Colors.transparent,
+              shape: const CircleBorder(),
+              clipBehavior: Clip.antiAlias,
+              child: InkWell(
+                // TODO implement onTap()
+                onTap: () {},
+                // TODO implement onLongPress()
+                onLongPress: () {},
+                splashColor: Colors.grey,
+                child: const Icon(Icons.send_outlined),
               ),
             ),
           ),
