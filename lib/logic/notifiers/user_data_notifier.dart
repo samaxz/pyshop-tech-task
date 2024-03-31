cc657ad9d3627fb611c66b8971d28c1ae36214c0
samax <samax.zer@gmail.com>
2024-03-31 17:07:33 +0300

    updated user data notifier

--- lib/logic/notifiers/user_data_notifier.dart
+++ lib/logic/notifiers/user_data_notifier.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:dio/dio.dart';
-import 'package:pyshop_task_camera_app/data/models/user_location.dart';
 import 'package:pyshop_task_camera_app/data/models/user_data.dart';
+import 'package:pyshop_task_camera_app/data/models/user_location.dart';
 import 'package:pyshop_task_camera_app/logic/notifiers/camera_permission_notifier.dart';
-import 'package:pyshop_task_camera_app/logic/providers/dio_provider.dart';
 import 'package:pyshop_task_camera_app/logic/providers/location_provider.dart';
 import 'package:pyshop_task_camera_app/logic/providers/text_controller_provider.dart';
+import 'package:pyshop_task_camera_app/logic/providers/user_data_service_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'user_data_notifier.g.dart';
@@ -17,12 +17,20 @@ part 'user_data_notifier.g.dart';
 class UserDataNotifier extends _$UserDataNotifier {
   @override
   AsyncValue<UserData> build() {
-    return const AsyncLoading();
+    // return const AsyncLoading();
+    return const AsyncData(
+      UserData(
+        text: 'initial text',
+        userLocation: UserLocation(
+          latitude: 228,
+          longitude: 69,
+        ),
+      ),
+    );
   }
 
   Future<UserLocation> _getUserLocation() async {
     final service = ref.read(locationServiceProvider);
-    // final location = await AsyncValue.guard(service.getUserLocation);
     final location = await service.getUserLocation();
     return location;
   }
@@ -48,81 +56,68 @@ class UserDataNotifier extends _$UserDataNotifier {
     }
   }
 
-  // convert photo to FormData so that it could be sent over the internet
-  Future<FormData> _convertPhotoToFormData() async {
+  // convert user data to FormData so that it could be sent over the internet
+  // in post request's body
+  Future<FormData> _convertUserDataToFormData() async {
     try {
+      final text = _getControllerText();
+      final location = await _getUserLocation();
       final photo = await _takePhoto();
       final convertedPhoto = File(photo.path);
       final multipartFile = await MultipartFile.fromFile(
         convertedPhoto.path,
         filename: 'test.jpg',
-        // filename: convertedPhoto.name,
       );
-      final formData = FormData.fromMap({'file': multipartFile});
-      return formData;
-      // could be thrown inside _takePhoto()
-    } on CameraException catch (e, st) {
-      log(
-        'CameraException caught inside _sendPhoto()',
-        error: e,
-        stackTrace: st,
-      );
-      rethrow;
-      // could be thrown inside MultipartFile.fromFile(..)
-    } on UnsupportedError catch (e, st) {
-      log(
-        'UnsupportedError caught inside _sendPhoto()',
-        error: e,
-        stackTrace: st,
-      );
-      rethrow;
-    }
-  }
-
-  Future<void> _sendUserData() async {
-    final text = _getControllerText();
-    final location = await _getUserLocation();
-    final photo = await _convertPhotoToFormData();
-    final url = Uri.https(
-      'flutter-sandbox.free.beeceptor.com',
-      '/upload_photo/',
-      {
+      final formData = FormData.fromMap({
         'comment': text,
         'latitude': location.latitude,
         'longitude': location.longitude,
-        'photo': photo,
-      },
-    ).toString();
-    final dio = ref.read(dioProvider);
-    final response = await dio.post(url, data: photo);
+        // not sure if i need to add @ here or whatever
+        'photo': multipartFile,
+      });
+      return formData;
+      // TODO remove all the commented catches
+      // } on DioException catch (e, st) {
+      // this gets caught inside _takePhoto(), so, i have no need in using it here
+      // on CameraException catch (e, st) {
+      //   log(
+      //     'CameraException caught inside _sendPhoto()',
+      //     error: e,
+      //     stackTrace: st,
+      //   );
+      //   rethrow;
+      // could be thrown inside MultipartFile.fromFile(..)
+    } on UnsupportedError catch (e, st) {
+      log(
+        'UnsupportedError caught inside _convertUserDataToFormData()',
+        error: e,
+        stackTrace: st,
+      );
+      rethrow;
+    }
   }
 
-  Future<void> uploadUserData() async {
+  Future<void> sendUserData() async {
     try {
-      final text = _getControllerText();
-      // log('this for sure gets here');
-      final userLocation = await _getUserLocation();
-      final photo = await _takePhoto();
-      // log('don`t you tell this gets executed');
-      log('photo path is: ${photo.path}');
+      if (state.isLoading) return;
+      state = const AsyncLoading();
+      // log('state is $state');
+      final formData = await _convertUserDataToFormData();
+      final service = ref.read(userDataServiceProvider);
+      final response = await service.uploadUserData(formData: formData);
       state = AsyncData(
         UserData(
-          text: text,
-          userLocation: userLocation,
-          photo: photo,
+          // this is 100% wrong, i'm pretty sure
+          text: formData.fields[0].value,
+          userLocation: UserLocation(
+            latitude: double.parse(formData.fields[1].value),
+            longitude: double.parse(formData.fields[2].value),
+          ),
         ),
       );
-    } on DioException catch (e, st) {
-      state = AsyncError(
-        e.message ?? 'unknown connection exception',
-        st,
-      );
-    } on CameraException catch (e, st) {
-      state = AsyncError(
-        e.description ?? 'unknown camera exception',
-        st,
-      );
+    } catch (e, st) {
+      state = AsyncError(e, st);
     }
-    log('notifier state: $state');
+    log('UserDataNotifier state: $state');
   }
 }
