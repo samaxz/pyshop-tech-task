04608622e7fdb0c64456a0c6aef42fec795e116b
samax <samax.zer@gmail.com>
2024-03-31 13:59:47 +0300

    refactored location service

---
+++ lib/logic/services/location_service.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:pyshop_task_camera_app/data/models/user_location.dart';
+
+class LocationService {
+  final Dio _dio;
+
+  const LocationService(this._dio);
+
+  static const _domain = 'api.bigdatacloud.net';
+  static const _endpoint = '/data/reverse-geocode-client';
+
+  Future<UserLocation> getUserLocation() async {
+    try {
+      final uri = Uri.https(_domain, _endpoint);
+      final response = await _dio.getUri(uri);
+      final data = Map<String, dynamic>.from(response.data);
+      final userLocation = UserLocation.fromJson(data);
+      return userLocation;
+    } on DioException catch (e, st) {
+      log(
+        'DioException caught inside getUserLocation()',
+        error: e.message,
+        stackTrace: st,
+      );
+      // Error.throwWithStackTrace(e, st);
+      rethrow;
+    }
+  }
+}
