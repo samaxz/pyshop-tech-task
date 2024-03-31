dac288289c072a0740fbc61863923bf842b064c8
samax <samax.zer@gmail.com>
2024-03-31 17:04:44 +0300

    added user data service

---
+++ lib/logic/services/user_data_service.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+class UserDataService {
+  final Dio _dio;
+
+  const UserDataService(this._dio);
+
+  static const _domain = 'flutter-sandbox.free.beeceptor.com';
+  static const _endpoint = '/upload_photo/';
+
+  Future<Response> uploadUserData({
+    required FormData formData,
+  }) async {
+    try {
+      final uri = Uri.https(_domain, _endpoint);
+      final response = await _dio.postUri(uri, data: formData);
+      return response;
+    } on DioException catch (e, st) {
+      log(
+        'DioException caught inside uploadUserData()',
+        error: e.message,
+        stackTrace: st,
+      );
+      rethrow;
+    }
+  }
+}
